a48ff35b379464dc35cfa53dda99f40fbdf2d434
ccanales2023 <orion.1781@gmail.com>
2023-07-11 17:15:43 -0500

    marcar como no nulo campos requeridos de la persona

---
+++ src/main/resources/marcar_como_no_nulo.sql
@@ -0,0 +1,4 @@
+ALTER TABLE tbl_persona MODIFY COLUMN apellido varchar(255) NOT NULL;
+ALTER TABLE tbl_persona MODIFY COLUMN nombre varchar(255) NOT NULL;
+ALTER TABLE tbl_persona MODIFY COLUMN numero_documento varchar(255) NOT NULL;
+ALTER TABLE tbl_persona MODIFY COLUMN tipo_documento varchar(255) NOT NULL;
