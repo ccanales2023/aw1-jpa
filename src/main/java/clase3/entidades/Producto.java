b6fc24953ad914f4420964a5b1b2f853b5f04e85
ccanales2023 <orion.1781@gmail.com>
2023-07-11 18:16:53 -0500

    clase producto creada

---
+++ src/main/java/clase3/entidades/Producto.java
@@ -0,0 +1,20 @@
+package clase3.entidades;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	long id;
+	
+	String nombre;
+	BigDecimal precio;
+	Integer stock;
+
+}
