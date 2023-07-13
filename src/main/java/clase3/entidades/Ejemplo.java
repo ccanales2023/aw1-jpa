7795328774dc55653820ad621873923ef4f39381
ccanales2023 <orion.1781@gmail.com>
2023-07-13 15:24:19 -0500

    Se creo la clase Marca y los Daos correspondientes

--- src/main/java/clase3/entidades/Ejemplo.java
+++ src/main/java/clase3/entidades/Ejemplo.java
@@ -1,5 +1,6 @@
 package clase3.entidades;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import javax.persistence.EntityManager;
@@ -9,19 +10,33 @@ import javax.persistence.Persistence;
 public class Ejemplo {
 	
 	public static void main (String[] args) {
-		Persona persona = new Persona();
-		persona.numeroDocumento = "73646447";
-		persona.tipoDocumento = TipoDocumento.DNI;
-		persona.nombre = "Arthur";
-		persona.apellido = "Mauricio";
+//		Persona persona = new Persona();
+//	    persona.numeroDocumento = "73646447";
+///		persona.tipoDocumento = TipoDocumento.DNI;
+//		persona.nombre = "Arthur";
+//		persona.apellido = "Mauricio";
 	
 
 
 
-		PersonaDao personaDao = new PersonaDao();
-		personaDao.save(persona);
+//		PersonaDao personaDao = new PersonaDao();
+//		personaDao.save(persona);
 
-		System.out.println("Se guardo en la BD");
+//		System.out.println("Se guardo en la BD");
+		
+		ProductoDao productoDao = new ProductoDao();
+		MarcaDao marcaDao = new MarcaDao();
+		
+		Marca panadol = new Marca();
+		panadol.nombre = "Panadol";
+		Marca panadolConId = marcaDao.save(panadol);
+		
+		Producto producto = new Producto();
+		producto.nombre = "Paracetamol";
+		producto.precio  = new BigDecimal(3.0);
+		producto.stock = 50;
+		producto.marca = panadolConId;
+		productoDao.save(producto);
 		
 		
 	}
--- src/main/java/clase3/entidades/Producto.java
+++ src/main/java/clase3/entidades/Producto.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 
 @Entity
 public class Producto {
@@ -17,5 +19,12 @@ public class Producto {
 	String descripcion;
 	BigDecimal precio;
 	Integer stock;
+	
+	//asociacion
+	//relacion
+	//MARCA_ID INTEGER HACIA AL MARCA
+	//JoinColumn
+	@ManyToOne(optional = true)
+	Marca marca;
 
 }
